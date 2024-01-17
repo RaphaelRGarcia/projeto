587e02943f7b3441dfc0b313b61679bcf1e27bc7
Raphael Garcia <raphaelreisflamengo@gmail.com>
2024-01-17 15:10:18 +0100

    remoção de imports

--- src/main/java/com/projeto/projeto/GreetingController.java
+++ src/main/java/com/projeto/projeto/GreetingController.java
@@ -1,11 +1,9 @@
 package com.projeto.projeto;
 
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
